df20612bb44dadf3396b03d9341741f4d25c496b
buba110 <112966658+buba110@users.noreply.github.com>
2026-04-06 13:31:58 -0600

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,9 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10
-});
+
+// Railway inyecta MYSQL_URL automáticamente
+const connectionUrl = process.env.MYSQL_URL || `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+const pool = mysql.createPool(connectionUrl);
 module.exports = pool.promise();
